feb84f062ddaae35579306d6ccc780f666f68eab
mubeensiddiqui786 <mubeenashfaq333@gmail.com>
2023-09-02 23:45:37 +0500

    added search service

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -22,13 +23,16 @@ namespace AuctionService.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(){
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
+        {
+            var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-            var auctions = await _context.Auctions
-                .Include(x => x.Item)
-                .OrderBy(x => x.Item.Make)
-                .ToListAsync();
-            return _mapper.Map<List<AuctionDto>>(auctions);
+            if (!string.IsNullOrWhiteSpace(date))
+            {
+                query = query.Where(x => x.CreatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+            }
+
+            return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
         }
 
         [HttpGet("{id}")]
